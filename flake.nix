e5748e09bd62908d18355c8e145d147d198c3a8b
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-08-01 19:41:48 -0300

    fix(deps): pin forked execution-zone stack

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
-    lez_core.url = "github:3esmit/logos-execution-zone-module?rev=c749def804bfba6c1104bf81e5bb500eb493a11c";
+    lez_core.url = "github:3esmit/logos-execution-zone-module?rev=e50f1628dff936b017ee2ec69e8c99b0cafb69a6";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
