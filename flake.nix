2612b9b5c72dedae3b41c9e4a88cf9e939e6a1d9
3esmit <3esmit@users.noreply.github.com>
2026-08-08 21:59:26 -0300

    fix(deps): use maintained module-builder fork

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Logos Execution Zone Wallet UI - QML view + C++ backend module";
 
   inputs = {
-    logos-module-builder.url = "github:logos-co/logos-module-builder";
+    logos-module-builder.url = "github:3esmit/logos-module-builder?rev=324b459c3f7b59171d249f3ccbcc362403b3fcaf";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
     lez_core.url = "github:3esmit/logos-execution-zone-module?rev=e50f1628dff936b017ee2ec69e8c99b0cafb69a6";
   };
